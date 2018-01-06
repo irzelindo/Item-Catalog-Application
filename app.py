53813d6a0380dcbc942420e989bdbb3c65aab985
Irzelindo Salvador <irzelindo.salvador@gmail.com>
2018-01-06 18:23:57 +0200

    update app

--- app.py
+++ app.py
@@ -437,5 +437,5 @@ def deleteItem(catalog_id, item_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
-    # app.run()
+    #app.run(host='0.0.0.0', port=8000)
+    app.run()
